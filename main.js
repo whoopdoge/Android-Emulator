4df98af943aef887ab7d52cf7090111bf6feefbf
whoopdoge <123603026+whoopdoge@users.noreply.github.com>
2024-03-08 13:27:50 -0800

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ document.querySelector('.loader').style.opacity = '0';
   // Delay to allow the proxy website to load
 setTimeout(function(
 ) {
-    // Replace 'https://example.com' with the URL of the website you want to load through the proxy
-const proxyObject = document.querySelector('#proxyObject');
-    proxyObject.setAttribute('data', 'https://now.gg/apps/uncube/7074/now.html');
+    // Replace 'https://now.gg/apps/uncube/7074/now.html' with the URL of the website you want to load through the proxy
+const proxyFrame = document.querySelector('#proxyFrame');
+    const urlToLoad = 'https://now.gg/apps/uncube/7074/now.html';
+    proxyFrame.src = urlToLoad;
   }, 2000); // Adjust the delay (2000ms = 2s) as needed
 });
